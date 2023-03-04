dabce2c7f4b7f5d288fd7d158082b8602f02dc6c
AriSmrd7 <arismrd13@gmail.com>
2023-03-04 07:42:33 +0700

    update view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.admin-master')
 
-@section('content')
+    @section('content')
+
+    @auth
     <div class="bg-light p-5 rounded">
         <h1>Halaman Utama</h1>
         <p class="lead">Selamat Datang di Halaman Admin</p>
     </div>
+    @endauth
+
+    @guest
+    <div class="bg-light p-5 rounded">
+        <h1>Data Tidak Dapat Diakses</h1>
+        <p class="lead">Silahkan login terlebih dahulu</p>
+    </div>
+    @endguest
+
 @endsection
